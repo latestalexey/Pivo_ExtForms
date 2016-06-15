37c50421b10dd11db65928bb6733d5d5ce98c788
Крапивин Андрей <krapivin@PIVZAVOZ.LOCAL>
2016-06-16 01:54:06 +0500

    Инициализация хранилища

---
+++ !Управление тороговлей/KassUpload.bat
@@ -0,0 +1,3 @@
+taskkill /fi "USERNAME eq PIVZAVOZ\ADMINISTRATOR" /im 1cv8.exe /f
+"C:\Program Files (x86)\1cv8\common\1cestart.exe" enterprise /S BRAIN.pivzavoz.local\pivzavoz_new  /N Reglam /P 911 /wa- /RunModeOrdinaryApplication /DisableStartupMessages /Execute "C:\1CWORK\KassUpload.epf"
+exit
